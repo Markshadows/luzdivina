a217ffb41f712df9940e5dfa7eb639d6472fbee9
markshadows <fracsteiger@gmail.com>
2018-12-16 16:36:07 -0300

    test branch api

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,5 @@ from .views import ComunidadDetalle,ComunidadLista,EventoComunidad
 
 urlpatterns = [
     url('comunidad_lista/',ComunidadLista.as_view()),
-    url(r'^comunidad_detalle/(?P<pk>[0-9]+)/$',ComunidadDetalle.as_view()),
     url('^comunidad_evento/(?P<comunidad>.+)/$', EventoComunidad.as_view()),
 ]
